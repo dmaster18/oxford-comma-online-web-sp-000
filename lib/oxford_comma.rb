c3199e7a56be07d0f3a4779b2972582c62378eb7
David Strauch <dmaster18@gmail.com>
2019-07-24 18:03:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   counter = 0
-  new_string_element
-  new_string
+  new_string_element = ""
+  new_string = ""
   while counter <= array.length - 2
     if counter == array.length - 2 
       new_string_element = array[counter].to_s + ", and "
