face682fe6bef5895a3219cb022f56d4277c89e5
David Strauch <dmaster18@gmail.com>
2019-07-24 17:53:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
   new_array = []
   new_string_element
   new_string
+  if 
   while counter < array.length - 2
     new_string_element = array[counter].to_s
     new_string = new_string_element + new_string
