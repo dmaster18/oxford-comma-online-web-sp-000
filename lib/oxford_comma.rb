f8f158e5dd4c7233e4779a8136923ad9aa1370a0
David Strauch <dmaster18@gmail.com>
2019-07-24 17:56:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ def oxford_comma(array)
     counter+=1
   end
   if counter == array.length - 2 
-      new_string_element = array[counter].to_s + ","
+      new_string_element = array[counter].to_s + ", and "
   if element != array.
         array.join(',')
     else
