c5f39771c876801424cb32a6aa883d3744161353
David Strauch <dmaster18@gmail.com>
2019-07-24 18:09:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,12 +6,11 @@ def oxford_comma(array)
     if counter == array.length - 2 
       new_string_element = array[counter].to_s << ", and "
       new_string = new_string_element << new_string
-      counter+=1
     else
       new_string_element = array[counter].to_s << ","
       new_string = new_string_element << new_string
-      counter+=1
     end
+          counter+=1
     return new_string
   end
 end
